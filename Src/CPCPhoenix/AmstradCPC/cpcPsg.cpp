c6abacc7df781b28045756e3f152e3fa15618e34
Arturo Colorado <arturocg@gmail.com>
2010-11-14 21:21:18 +0000

    Nuevo ejecutable.

--- Src/CPCPhoenix/AmstradCPC/cpcPsg.cpp
+++ Src/CPCPhoenix/AmstradCPC/cpcPsg.cpp
@@ -64,31 +64,16 @@ namespace CPC {
 
       case FUNCTION_READ_REGISTER:
         // We do nothing right now, CPpi will eventually call CPsg::GetSelectedRegisterValue to actually read the selected register.
-        {
-          char szBuffer[100];
-          _snprintf( szBuffer, sizeof(szBuffer), "PSG: Read register %d\n", m_nSelectedRegister );
-          OutputDebugString( szBuffer );
-        }
         break;
 
       case FUNCTION_WRITE_REGISTER:
         // We take the value from PPI port A and write it to the currently selected PSG register.
         m_anRegisters[m_nSelectedRegister] = GetMachine()->GetPpi()->GetPortOutputValue( CPpi::PORT_A );
-        {
-          char szBuffer[100];
-          _snprintf( szBuffer, sizeof(szBuffer), "PSG: Write %d into register %d\n", GetMachine()->GetPpi()->GetPortOutputValue(CPpi::PORT_A), m_nSelectedRegister );
-          OutputDebugString( szBuffer );
-        }
         break;
 
       case FUNCTION_SELECT_REGISTER:
         // We take the register index from PPI port A and remember it for subsequent register reads/writes.
         m_nSelectedRegister = (unsigned) (GetMachine()->GetPpi()->GetPortOutputValue( CPpi::PORT_A ) & 0x0F);   // Bits 3-0 of PPI port A value contain the register index.
-        {
-          char szBuffer[100];
-          _snprintf( szBuffer, sizeof(szBuffer), "PSG: Select register %d\n", m_nSelectedRegister );
-          OutputDebugString( szBuffer );
-        }
     }
   }
 
