bfef6600eb5ad44b358505cda9717ecbfa42b7f6
Arturo Colorado <arturocg@gmail.com>
2011-07-24 17:38:35 +0000

    Cambios en los atajos de teclado: Shift+F5 para reset y F3/F4 para velocidad de emulación 100%/ilimitada.

--- Src/CPCPhoenix/res/Resource.h
+++ Src/CPCPhoenix/res/Resource.h
@@ -45,7 +45,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32814
+#define _APS_NEXT_COMMAND_VALUE         32817
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           102
 #endif
