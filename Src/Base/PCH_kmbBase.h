43bb82e357af884158de6afe6bfb43c5ee1f3095
Arturo Colorado <arturocg@gmail.com>
2016-01-07 19:13:34 +0000

    Solution updated to Visual Studio 2015.

--- Src/Base/PCH_kmbBase.h
+++ Src/Base/PCH_kmbBase.h
@@ -21,10 +21,8 @@
 #include <vector>
 #include <list>
 #include <algorithm>
-#include <hash_map>
 
 using namespace std;    // No need to specify std:: for STL symbols
-using namespace stdext; // No need to specify stdext:: for STL symbols (hash_map, hash_set)
 
 // Standard C includes
 #include <stdio.h>
