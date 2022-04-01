327db6bc1164d3752042cb9d44aa0d861e0ddbf0
Arturo Colorado <arturocg@gmail.com>
2022-04-01 22:01:39 +0200

    Code reformat.

--- Src/AmstradCPC/cpcPsg.cpp
+++ Src/AmstradCPC/cpcPsg.cpp
@@ -19,222 +19,222 @@ static const float PI = 3.1415926535897932384626433832795f;
 namespace CPC {
 
 
-  //----------------------------------------------------------------------------
-  /**
-  ** 
-  */
-  CPsg::CPsg(CMachine *pMachine) : inherited( pMachine )
-  {
-    // Reset members
-    ResetVars();
-
-    //...
-  }
-
-  //----------------------------------------------------------------------------
-  /**
-  ** ResetVars
-  */
-  void CPsg::ResetVars()
-  {
-    m_eSelectedRegister = REG_A_TONE_PERIOD_LOW;
-    m_fAccumCycles      = 0.f;
-    m_fAngle            = 0.f;
-  }
-
-  //----------------------------------------------------------------------------
-  /**
-  ** FreeVars
-  */
-  void CPsg::FreeVars()
-  {
-    //...
-  }
-
-  //----------------------------------------------------------------------------
-  /**
-  ** 
-  */
-  /*virtual*/ void CPsg::Reset()
-  {
-    ResetVars();
-  }
-
-  //----------------------------------------------------------------------------
-  /**
-  ** 
-  */
-  void CPsg::SelectFunction(EFunction eFunction)
-  {
-    switch (eFunction)
+    //----------------------------------------------------------------------------
+    /**
+    **
+    */
+    CPsg::CPsg(CMachine* pMachine) : inherited(pMachine)
     {
-      case FUNCTION_INACTIVE:
-        // Inactive - Do nothing.
-        break;
+        // Reset members
+        ResetVars();
 
-      case FUNCTION_READ_REGISTER:
-        // We do nothing right now, CPpi will eventually call CPsg::GetSelectedRegisterValue to actually read the selected register.
-        break;
-
-      case FUNCTION_WRITE_REGISTER:
-        // We take the value from PPI port A and write it to the currently selected PSG register.
-        m_anRegisters[m_eSelectedRegister] = GetMachine()->GetPpi()->GetPortOutputValue( CPpi::PORT_A );
-        break;
-
-      case FUNCTION_SELECT_REGISTER:
-        // We take the register index from PPI port A and remember it for subsequent register reads/writes.
-        m_eSelectedRegister = (ERegister) (GetMachine()->GetPpi()->GetPortOutputValue( CPpi::PORT_A ) & 0x0F);   // Bits 3-0 of PPI port A value contain the register index.
-    }
-  }
-
-  //----------------------------------------------------------------------------
-  /**
-  ** 
-  */
-  float CPsg::GenerateNoiseSample()
-  {
-    // 1. Generate white noise (i.e. random number that is either -1 or 1, both with same probability to be picked).
-    // 2. Modulate it with the programmed frequency.
-    float fNoise = ( rand()<(RAND_MAX/2) ? -1.f : 1.f ) * 0.2f/*noise amplitude*/;
-
-    //////unsigned nNoisePeriod = m_anRegisters[REG_NOISE_PERIOD] & 0x1F;
-    //////float fNoisePeriod = float( nNoisePeriod!=0 ? nNoisePeriod : 1 );
-    //////float fFrequency = 1000000.f / (16.f * fNoisePeriod);                          // Formula from manufacturer's chip datasheet.
-    //////float fRet = ::sinf( (fFrequency + fNoise) * m_fAngle );
-    //////return fRet;
-    return fNoise;
-  }
-
-  //----------------------------------------------------------------------------
-  /**
-  ** 
-  */
-  float CPsg::GenerateSample(unsigned nTonePeriod, unsigned nFixedAmplitude, float fNoiseSample, int/*EGenerateSampleFlags*/ nFlags)
-  {
-    float fRet = 0.f;
-
-    // Tone
-    if (nFlags & GENSAMPLE_TONE_ENABLED)
-    {
-      float fTonePeriod = float( nTonePeriod!=0 ? nTonePeriod : 1 );
-      float fFrequency = 1000000.f / (16.f * fTonePeriod);                          // Formula from manufacturer's chip datasheet.
-
-      fRet = ::sinf( fFrequency * m_fAngle );
-      fRet = ( fRet<0.f ? -1.f : 1.f );     // Convert to square wave
+        //...
     }
 
-    // Mix tone and noise
-    if ( (nFlags & GENSAMPLE_TONE_ENABLED) &&
-         (nFlags & GENSAMPLE_NOISE_ENABLED) )
+    //----------------------------------------------------------------------------
+    /**
+    ** ResetVars
+    */
+    void CPsg::ResetVars()
     {
-      // Tone + noise
-      fRet = (fRet + fNoiseSample) * 0.5f;
-    }
-    else if (nFlags & GENSAMPLE_TONE_ENABLED)
-    {
-      // Tone only
-      // Nothing to do, fRet already contains the tone sample
-    }
-    else if (nFlags & GENSAMPLE_NOISE_ENABLED)
-    {
-      // Noise only
-      fRet = fNoiseSample;
-    }
-    else
-    {
-      // None of them enabled. The PSG outputs 1.f in this case.
-      // This, in combination with amplitude control, is used by some software to play raw PCM data (e.g. digitized voice, sampled music, etc.).
-      fRet = 1.f;
+        m_eSelectedRegister = REG_A_TONE_PERIOD_LOW;
+        m_fAccumCycles = 0.f;
+        m_fAngle = 0.f;
     }
 
-    // Amplitude
-    // TODO: Envelopes
-    float fAmplitude = ( (nFlags&GENSAMPLE_USE_ENVELOPE) ? 1.f :                                //***** TODO - TODO - TODO *****
-                                                           float(nFixedAmplitude) / 15.f );     // nFixedAmplitude is in the range [0,15].
-    fRet *= fAmplitude;
-
-    return fRet;
-  }
-
-  //----------------------------------------------------------------------------
-  /**
-  ** 
-  */
-  float CPsg::GenerateChannelSample(unsigned nRegToneLow, unsigned nRegToneHigh, unsigned nRegAmplitude, unsigned nMixerOffset, float fNoiseSample)
-  {
-    float fRet = 0.f;
-
-    int nFlags = 0;
-    if ((m_anRegisters[REG_MIXER] & (0x01<<nMixerOffset)) == 0)
+    //----------------------------------------------------------------------------
+    /**
+    ** FreeVars
+    */
+    void CPsg::FreeVars()
     {
-      nFlags |= GENSAMPLE_TONE_ENABLED;
-    }
-    if ((m_anRegisters[REG_MIXER] & (0x08<<nMixerOffset)) == 0)
-    {
-      nFlags |= GENSAMPLE_NOISE_ENABLED;
-    }
-    if ((m_anRegisters[nRegAmplitude]&0x10) != 0)
-    {
-      nFlags |= GENSAMPLE_USE_ENVELOPE;
+        //...
     }
 
-    if ((nFlags & (GENSAMPLE_TONE_ENABLED | GENSAMPLE_NOISE_ENABLED)) != 0)   // If the channel is active...
+    //----------------------------------------------------------------------------
+    /**
+    **
+    */
+    /*virtual*/ void CPsg::Reset()
     {
-      fRet = GenerateSample( ((m_anRegisters[nRegToneHigh]&0x0F) << 8) | m_anRegisters[nRegToneLow],
-                             m_anRegisters[nRegAmplitude] & 0x0F,
-                             fNoiseSample,
-                             nFlags );
+        ResetVars();
     }
 
-    return fRet;
-  }
-
-  //----------------------------------------------------------------------------
-  /**
-  ** 
-  */
-  void CPsg::Run(unsigned nNumCycles)
-  {
-    if (GetMachine()->GetSoundOutput() != NULL)
+    //----------------------------------------------------------------------------
+    /**
+    **
+    */
+    void CPsg::SelectFunction(EFunction eFunction)
     {
-      m_fAccumCycles += (float) nNumCycles;
-
-      // Generate sound samples while there are enough accumulated cycles
-      while (m_fAccumCycles >= CYCLES_PER_SAMPLE)
-      {
-        // Noise generation
-        float fNoiseSample = GenerateNoiseSample();
-
-        // Generate a sample for each channel
-        float fSampleA;
-        float fSampleB;
-        float fSampleC;
-        fSampleA = GenerateChannelSample( REG_A_TONE_PERIOD_LOW, REG_A_TONE_PERIOD_HIGH, REG_A_AMPLITUDE, 0, fNoiseSample );
-        fSampleB = GenerateChannelSample( REG_B_TONE_PERIOD_LOW, REG_B_TONE_PERIOD_HIGH, REG_B_AMPLITUDE, 1, fNoiseSample );
-        fSampleC = GenerateChannelSample( REG_C_TONE_PERIOD_LOW, REG_C_TONE_PERIOD_HIGH, REG_C_AMPLITUDE, 2, fNoiseSample );
-
-        // Mix samples from each channel and write the resulting sample to the sound output
-        float fSample;
-        fSample = (fSampleA + fSampleB + fSampleC) / 3.f/*num channels*/;
-
-        //////float fNoiseOffset = float(rand()) / float(RAND_MAX) * 10.f/*noise factor*/;
-
-        //////float fSinParam;
-        //////fSinParam = fmod( m_fAngle * (500.f + fNoiseOffset), 2.f * PI );
-
-        //////float fSample = ::sinf( fSinParam );
-
-        GetMachine()->GetSoundOutput()->WriteSample( fSample );
-
-        // Update cycle accumulator, angle, etc.
-        m_fAccumCycles -= CYCLES_PER_SAMPLE;
-        m_fAngle += ANGLE_INC_PER_SAMPLE;
-        while (m_fAngle >= 2.f * PI)
+        switch (eFunction)
         {
-          m_fAngle -= 2.f * PI;
+            case FUNCTION_INACTIVE:
+                // Inactive - Do nothing.
+                break;
+
+            case FUNCTION_READ_REGISTER:
+                // We do nothing right now, CPpi will eventually call CPsg::GetSelectedRegisterValue to actually read the selected register.
+                break;
+
+            case FUNCTION_WRITE_REGISTER:
+                // We take the value from PPI port A and write it to the currently selected PSG register.
+                m_anRegisters[m_eSelectedRegister] = GetMachine()->GetPpi()->GetPortOutputValue(CPpi::PORT_A);
+                break;
+
+            case FUNCTION_SELECT_REGISTER:
+                // We take the register index from PPI port A and remember it for subsequent register reads/writes.
+                m_eSelectedRegister = (ERegister)(GetMachine()->GetPpi()->GetPortOutputValue(CPpi::PORT_A) & 0x0F);   // Bits 3-0 of PPI port A value contain the register index.
         }
-      }
     }
-  }
+
+    //----------------------------------------------------------------------------
+    /**
+    **
+    */
+    float CPsg::GenerateNoiseSample()
+    {
+        // 1. Generate white noise (i.e. random number that is either -1 or 1, both with same probability to be picked).
+        // 2. Modulate it with the programmed frequency.
+        float fNoise = (rand() < (RAND_MAX / 2) ? -1.f : 1.f) * 0.2f/*noise amplitude*/;
+
+        //////unsigned nNoisePeriod = m_anRegisters[REG_NOISE_PERIOD] & 0x1F;
+        //////float fNoisePeriod = float( nNoisePeriod!=0 ? nNoisePeriod : 1 );
+        //////float fFrequency = 1000000.f / (16.f * fNoisePeriod);                          // Formula from manufacturer's chip datasheet.
+        //////float fRet = ::sinf( (fFrequency + fNoise) * m_fAngle );
+        //////return fRet;
+        return fNoise;
+    }
+
+    //----------------------------------------------------------------------------
+    /**
+    **
+    */
+    float CPsg::GenerateSample(unsigned nTonePeriod, unsigned nFixedAmplitude, float fNoiseSample, int/*EGenerateSampleFlags*/ nFlags)
+    {
+        float fRet = 0.f;
+
+        // Tone
+        if (nFlags & GENSAMPLE_TONE_ENABLED)
+        {
+            float fTonePeriod = float(nTonePeriod != 0 ? nTonePeriod : 1);
+            float fFrequency = 1000000.f / (16.f * fTonePeriod);                          // Formula from manufacturer's chip datasheet.
+
+            fRet = ::sinf(fFrequency * m_fAngle);
+            fRet = (fRet < 0.f ? -1.f : 1.f);     // Convert to square wave
+        }
+
+        // Mix tone and noise
+        if ((nFlags & GENSAMPLE_TONE_ENABLED) &&
+            (nFlags & GENSAMPLE_NOISE_ENABLED))
+        {
+            // Tone + noise
+            fRet = (fRet + fNoiseSample) * 0.5f;
+        }
+        else if (nFlags & GENSAMPLE_TONE_ENABLED)
+        {
+            // Tone only
+            // Nothing to do, fRet already contains the tone sample
+        }
+        else if (nFlags & GENSAMPLE_NOISE_ENABLED)
+        {
+            // Noise only
+            fRet = fNoiseSample;
+        }
+        else
+        {
+            // None of them enabled. The PSG outputs 1.f in this case.
+            // This, in combination with amplitude control, is used by some software to play raw PCM data (e.g. digitized voice, sampled music, etc.).
+            fRet = 1.f;
+        }
+
+        // Amplitude
+        // TODO: Envelopes
+        float fAmplitude = ((nFlags & GENSAMPLE_USE_ENVELOPE) ? 1.f :                               //***** TODO - TODO - TODO *****
+                                                                float(nFixedAmplitude) / 15.f);     // nFixedAmplitude is in the range [0,15].
+        fRet *= fAmplitude;
+
+        return fRet;
+    }
+
+    //----------------------------------------------------------------------------
+    /**
+    **
+    */
+    float CPsg::GenerateChannelSample(unsigned nRegToneLow, unsigned nRegToneHigh, unsigned nRegAmplitude, unsigned nMixerOffset, float fNoiseSample)
+    {
+        float fRet = 0.f;
+
+        int nFlags = 0;
+        if ((m_anRegisters[REG_MIXER] & (0x01 << nMixerOffset)) == 0)
+        {
+            nFlags |= GENSAMPLE_TONE_ENABLED;
+        }
+        if ((m_anRegisters[REG_MIXER] & (0x08 << nMixerOffset)) == 0)
+        {
+            nFlags |= GENSAMPLE_NOISE_ENABLED;
+        }
+        if ((m_anRegisters[nRegAmplitude] & 0x10) != 0)
+        {
+            nFlags |= GENSAMPLE_USE_ENVELOPE;
+        }
+
+        if ((nFlags & (GENSAMPLE_TONE_ENABLED | GENSAMPLE_NOISE_ENABLED)) != 0)   // If the channel is active...
+        {
+            fRet = GenerateSample(((m_anRegisters[nRegToneHigh] & 0x0F) << 8) | m_anRegisters[nRegToneLow],
+                                  m_anRegisters[nRegAmplitude] & 0x0F,
+                                  fNoiseSample,
+                                  nFlags);
+        }
+
+        return fRet;
+    }
+
+    //----------------------------------------------------------------------------
+    /**
+    **
+    */
+    void CPsg::Run(unsigned nNumCycles)
+    {
+        if (GetMachine()->GetSoundOutput() != NULL)
+        {
+            m_fAccumCycles += (float)nNumCycles;
+
+            // Generate sound samples while there are enough accumulated cycles
+            while (m_fAccumCycles >= CYCLES_PER_SAMPLE)
+            {
+                // Noise generation
+                float fNoiseSample = GenerateNoiseSample();
+
+                // Generate a sample for each channel
+                float fSampleA;
+                float fSampleB;
+                float fSampleC;
+                fSampleA = GenerateChannelSample(REG_A_TONE_PERIOD_LOW, REG_A_TONE_PERIOD_HIGH, REG_A_AMPLITUDE, 0, fNoiseSample);
+                fSampleB = GenerateChannelSample(REG_B_TONE_PERIOD_LOW, REG_B_TONE_PERIOD_HIGH, REG_B_AMPLITUDE, 1, fNoiseSample);
+                fSampleC = GenerateChannelSample(REG_C_TONE_PERIOD_LOW, REG_C_TONE_PERIOD_HIGH, REG_C_AMPLITUDE, 2, fNoiseSample);
+
+                // Mix samples from each channel and write the resulting sample to the sound output
+                float fSample;
+                fSample = (fSampleA + fSampleB + fSampleC) / 3.f/*num channels*/;
+
+                //////float fNoiseOffset = float(rand()) / float(RAND_MAX) * 10.f/*noise factor*/;
+
+                //////float fSinParam;
+                //////fSinParam = fmod( m_fAngle * (500.f + fNoiseOffset), 2.f * PI );
+
+                //////float fSample = ::sinf( fSinParam );
+
+                GetMachine()->GetSoundOutput()->WriteSample(fSample);
+
+                // Update cycle accumulator, angle, etc.
+                m_fAccumCycles -= CYCLES_PER_SAMPLE;
+                m_fAngle += ANGLE_INC_PER_SAMPLE;
+                while (m_fAngle >= 2.f * PI)
+                {
+                    m_fAngle -= 2.f * PI;
+                }
+            }
+        }
+    }
 
 } //namespace CPC
--- Src/AmstradCPC/cpcPsg.h
+++ Src/AmstradCPC/cpcPsg.h
@@ -11,102 +11,102 @@
 namespace CPC {
 
 
-  /**
-  ** The AY-3-8912 Programmable Sound Generator (PSG), made by General Instruments, is the responsible for generating sound in the Amstrad CPC.
-  ** In addition, the CPC uses the PSG to scan the keyboard matrix to read the state of the keyboard and joysticks.
-  **
-  ** In the Amstrad CPC, The AY-3-8912 PSG is accessed through the 8255 PPI.
-  **
-  ** Datasheet: http://www.cpcwiki.eu/imgs/d/dc/Ay3-891x.pdf
-  */
-  class CPsg : public CSubSystem
-  {
-  public:
-
-    enum EFunction
+    /**
+    ** The AY-3-8912 Programmable Sound Generator (PSG), made by General Instruments, is the responsible for generating sound in the Amstrad CPC.
+    ** In addition, the CPC uses the PSG to scan the keyboard matrix to read the state of the keyboard and joysticks.
+    **
+    ** In the Amstrad CPC, The AY-3-8912 PSG is accessed through the 8255 PPI.
+    **
+    ** Datasheet: http://www.cpcwiki.eu/imgs/d/dc/Ay3-891x.pdf
+    */
+    class CPsg : public CSubSystem
     {
-      FUNCTION_INACTIVE        = 0x00,
-      FUNCTION_READ_REGISTER   = 0x01,    // Read from the selected PSG register. The register content is then readable from PPI port A.
-      FUNCTION_WRITE_REGISTER  = 0x02,    // Write to the selected PSG register. The register content is taken from PPI port A.
-      FUNCTION_SELECT_REGISTER = 0x03,    // Selects the specified PSG register. The register to select is taken from PPI port A.
+    public:
 
-      FUNCTION_LAST,
-      FUNCTION_INVALID = 0x7FFFFFFF
+        enum EFunction
+        {
+            FUNCTION_INACTIVE = 0x00,
+            FUNCTION_READ_REGISTER = 0x01,      // Read from the selected PSG register. The register content is then readable from PPI port A.
+            FUNCTION_WRITE_REGISTER = 0x02,     // Write to the selected PSG register. The register content is taken from PPI port A.
+            FUNCTION_SELECT_REGISTER = 0x03,    // Selects the specified PSG register. The register to select is taken from PPI port A.
+
+            FUNCTION_LAST,
+            FUNCTION_INVALID = 0x7FFFFFFF
+        };
+
+        enum ERegister
+        {
+            REG_A_TONE_PERIOD_LOW = 0x00,       // [Channel A] Lowest 8 bits of the period of the generated square wave.
+            REG_A_TONE_PERIOD_HIGH = 0x01,      // [Channel A] Highest 4 bits of the period of the generated square wave.
+            REG_B_TONE_PERIOD_LOW = 0x02,       // [Channel B] Lowest 8 bits of the period of the generated square wave.
+            REG_B_TONE_PERIOD_HIGH = 0x03,      // [Channel B] Highest 4 bits of the period of the generated square wave.
+            REG_C_TONE_PERIOD_LOW = 0x04,       // [Channel C] Lowest 8 bits of the period of the generated square wave.
+            REG_C_TONE_PERIOD_HIGH = 0x05,      // [Channel C] Highest 4 bits of the period of the generated square wave.
+            REG_NOISE_PERIOD = 0x06,            // Period of the noise (lower 5 bits). The generated noise can be mixed into any of the three channels.
+            REG_MIXER = 0x07,                   // Enables or disables tone and noise generation on each channel.
+            REG_A_AMPLITUDE = 0x08,             // [Channel A] If bit 4 is set, amplitude is controlled by the envelope. If bit 4 is clear, bits 3-0 determine a constant amplitude.
+            REG_B_AMPLITUDE = 0x09,             // [Channel B] If bit 4 is set, amplitude is controlled by the envelope. If bit 4 is clear, bits 3-0 determine a constant amplitude.
+            REG_C_AMPLITUDE = 0x0A,             // [Channel C] If bit 4 is set, amplitude is controlled by the envelope. If bit 4 is clear, bits 3-0 determine a constant amplitude.
+            REG_ENVELOPE_PERIOD_LOW = 0x0B,     // Lowest 8 bits of the period of the envelope.
+            REG_ENVELOPE_PERIOD_HIGH = 0x0C,    // Highest 8 bits of the period of the envelope.
+            REG_ENVELOPE_SHAPE = 0x0D,          // Envelope shape (see data sheet).
+            REG_IO_PORT = 0x0E,                 // I/O Port A (the only one in the AY-3-8912). In the CPC, it is connected to the selected keyboard matrix.
+
+            REG_COUNT
+        };
+
+
+        CPsg(CMachine* pMachine);
+        virtual                ~CPsg() { FreeVars(); }
+
+        /** Resets the subsystem. */
+        virtual void            Reset();
+
+        /** Selects the PSG function to perform. */
+        void                    SelectFunction(EFunction eFunction);
+
+        /** Writes a new value into the specified register.
+        *** This is used by CKeyboard each time a new keyboard matrix line is selected or a key is pressed or released. */
+        void                    SetRegisterValue(ERegister eRegister, cpcByte nValue) { m_anRegisters[eRegister] = nValue; }
+        /** Writes a new value into the currently selected register. */
+        void                    SetSelectedRegisterValue(cpcByte nValue) { m_anRegisters[m_eSelectedRegister] = nValue; }
+        /** Returns the value of the currently selected value. */
+        cpcByte                 GetSelectedRegisterValue() const { return m_anRegisters[m_eSelectedRegister]; }
+
+        /** Runs the PSG for the given number of cycles. */
+        void                    Run(unsigned nNumCycles);
+
+
+    private:
+
+        typedef                 CSubSystem                inherited;
+
+        static const float      CYCLES_PER_SAMPLE;        // Every how many cycles we need to generate a sample (chip_clock/sample_rate = 1Mhz/44.1kHz).
+        static const float      ANGLE_INC_PER_SAMPLE;     // Angle increment at every sample.
+
+        enum EGenerateSampleFlags
+        {
+            GENSAMPLE_TONE_ENABLED = 0x01,      // Mix tone wave in.
+            GENSAMPLE_NOISE_ENABLED = 0x02,     // Min noise wave in.
+            GENSAMPLE_USE_ENVELOPE = 0x04,      // Amplitude is controlled by the envelope.
+        };
+
+
+        void                    ResetVars();
+        void                    FreeVars();
+
+        float                   GenerateNoiseSample();
+        float                   GenerateChannelSample(unsigned nRegToneLow, unsigned nRegToneHigh, unsigned nRegAmplitude, unsigned nMixerOffset, float fNoiseSample);
+        float                   GenerateSample(unsigned nTonePeriod, unsigned nFixedAmplitude, float fNoiseSample, int/*EGenerateSampleFlags*/ nFlags);
+
+
+        ERegister               m_eSelectedRegister;
+        cpcByte                 m_anRegisters[REG_COUNT];
+
+        float                   m_fAccumCycles;           // Used to determine when to compute a new sound sample.
+        float                   m_fAngle;
+
     };
-
-    enum ERegister
-    {
-      REG_A_TONE_PERIOD_LOW    = 0x00,         // [Channel A] Lowest 8 bits of the period of the generated square wave.
-      REG_A_TONE_PERIOD_HIGH   = 0x01,         // [Channel A] Highest 4 bits of the period of the generated square wave.
-      REG_B_TONE_PERIOD_LOW    = 0x02,         // [Channel B] Lowest 8 bits of the period of the generated square wave.
-      REG_B_TONE_PERIOD_HIGH   = 0x03,         // [Channel B] Highest 4 bits of the period of the generated square wave.
-      REG_C_TONE_PERIOD_LOW    = 0x04,         // [Channel C] Lowest 8 bits of the period of the generated square wave.
-      REG_C_TONE_PERIOD_HIGH   = 0x05,         // [Channel C] Highest 4 bits of the period of the generated square wave.
-      REG_NOISE_PERIOD         = 0x06,         // Period of the noise (lower 5 bits). The generated noise can be mixed into any of the three channels.
-      REG_MIXER                = 0x07,         // Enables or disables tone and noise generation on each channel.
-      REG_A_AMPLITUDE          = 0x08,         // [Channel A] If bit 4 is set, amplitude is controlled by the envelope. If bit 4 is clear, bits 3-0 determine a constant amplitude.
-      REG_B_AMPLITUDE          = 0x09,         // [Channel B] If bit 4 is set, amplitude is controlled by the envelope. If bit 4 is clear, bits 3-0 determine a constant amplitude.
-      REG_C_AMPLITUDE          = 0x0A,         // [Channel C] If bit 4 is set, amplitude is controlled by the envelope. If bit 4 is clear, bits 3-0 determine a constant amplitude.
-      REG_ENVELOPE_PERIOD_LOW  = 0x0B,         // Lowest 8 bits of the period of the envelope.
-      REG_ENVELOPE_PERIOD_HIGH = 0x0C,         // Highest 8 bits of the period of the envelope.
-      REG_ENVELOPE_SHAPE       = 0x0D,         // Envelope shape (see data sheet).
-      REG_IO_PORT              = 0x0E,         // I/O Port A (the only one in the AY-3-8912). In the CPC, it is connected to the selected keyboard matrix.
-
-      REG_COUNT
-    };
-
-
-                            CPsg                      (CMachine *pMachine);
-    virtual                ~CPsg                      ()  { FreeVars(); }
-
-    /** Resets the subsystem. */
-    virtual void            Reset                     ();
-
-    /** Selects the PSG function to perform. */
-    void                    SelectFunction            (EFunction eFunction);
-
-    /** Writes a new value into the specified register.
-    *** This is used by CKeyboard each time a new keyboard matrix line is selected or a key is pressed or released. */
-    void                    SetRegisterValue          (ERegister eRegister, cpcByte nValue)  { m_anRegisters[eRegister] = nValue; }
-    /** Writes a new value into the currently selected register. */
-    void                    SetSelectedRegisterValue  (cpcByte nValue)                 { m_anRegisters[m_eSelectedRegister] = nValue; }
-    /** Returns the value of the currently selected value. */
-    cpcByte                 GetSelectedRegisterValue  () const                         { return m_anRegisters[m_eSelectedRegister]; }
-
-    /** Runs the PSG for the given number of cycles. */
-    void                    Run                       (unsigned nNumCycles);
-
-
-  private:
-
-    typedef                 CSubSystem                inherited;
-
-    static const float      CYCLES_PER_SAMPLE;        // Every how many cycles we need to generate a sample (chip_clock/sample_rate = 1Mhz/44.1kHz).
-    static const float      ANGLE_INC_PER_SAMPLE;     // Angle increment at every sample.
-
-    enum EGenerateSampleFlags
-    {
-      GENSAMPLE_TONE_ENABLED  = 0x01,      // Mix tone wave in.
-      GENSAMPLE_NOISE_ENABLED = 0x02,      // Min noise wave in.
-      GENSAMPLE_USE_ENVELOPE  = 0x04,      // Amplitude is controlled by the envelope.
-    };
-
-
-    void                    ResetVars                 ();
-    void                    FreeVars                  ();
-
-    float                   GenerateNoiseSample       ();
-    float                   GenerateChannelSample     (unsigned nRegToneLow, unsigned nRegToneHigh, unsigned nRegAmplitude, unsigned nMixerOffset, float fNoiseSample);
-    float                   GenerateSample            (unsigned nTonePeriod, unsigned nFixedAmplitude, float fNoiseSample, int/*EGenerateSampleFlags*/ nFlags);
-
-
-    ERegister               m_eSelectedRegister;
-    cpcByte                 m_anRegisters[REG_COUNT];
-
-    float                   m_fAccumCycles;           // Used to determine when to compute a new sound sample.
-    float                   m_fAngle;
-
-  };
 
 
 } //namespace CPC
