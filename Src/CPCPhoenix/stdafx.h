4ba40923c59cf6772a74a78d364b3fc2061e5b59
Arturo Colorado <arturocg@gmail.com>
2026-03-24 21:48:34 +0100

    Use language standard C++17

--- Src/CPCPhoenix/stdafx.h
+++ Src/CPCPhoenix/stdafx.h
@@ -51,6 +51,8 @@
 
 
 // STL includes
+// TODO: Remove when "using namespace std" is removed. Prevents C++17 std::byte from conflicting with Windows SDK's byte typedef.
+#define _HAS_STD_BYTE 0
 #include <string>
 #include <vector>
 #include <list>
